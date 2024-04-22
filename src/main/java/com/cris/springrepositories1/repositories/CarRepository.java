af709997fb74c1455c484e65fbda32a66b12cb14
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-04-22 12:31:42 +0200

    creazione programma

---
+++ src/main/java/com/cris/springrepositories1/repositories/CarRepository.java
@@ -0,0 +1,9 @@
+package com.cris.springrepositories1.repositories;
+
+import com.cris.springrepositories1.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
